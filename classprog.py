bc5517224115c7fa6b48eff08f707727035467b9
Mikask <darrenprasetya40@gmail.com>
2021-08-22 17:06:26 +0800

    Added calculation for the 1st and 2nd field

--- classprog.py
+++ classprog.py
@@ -1,55 +1,56 @@
-#pengabdian masyarakat
+# pengabdian masyarakat
 peng_mas = {
-    "peran":["Anggota","Panitia Inti/Asisten","Ketua"],
-    "pelaksanaan":["Individu","Kelompok"],
-    "rentang_waktu":["<1 Bulan","Sd. 3 Bulan","Sd. 6 Bulan","Sd. 1 Tahun"],
-    "nilai":[">25 Orang","Sd. 25 Orang","Sd. 10 Orang","Individu"]
-    }
+    "peran": ["Anggota", "Panitia Inti/Asisten", "Ketua"],
+    "pelaksanaan": ["Individu", "Kelompok"],
+    "rentang_waktu": ["<1 Bulan", "Sd. 3 Bulan", "Sd. 6 Bulan", "Sd. 1 Tahun"],
+    "nilai": [">25 Orang", "Sd. 25 Orang", "Sd. 10 Orang", "Individu"]
+}
 
-#Internsionalisasi, Summit, dan Rekognisi
+# Internsionalisasi, Summit, dan Rekognisi
 isr = {
-    "pelaksanaan":["Individu","Kelompok Sd. 3 Orang","Kelompok >3 Orang","Pengurus"],
-    "tingkat":["","Nasional","Internasional"],
-    "disiplin":["Tidak Berhubungan","Berhubungan"],
-    "rentang_waktu":["<1 Bulan","Sd. 3 Bulan","Sd. 6 Bulan","Sd. 1 Tahun",">1 Tahun"],
-    "nilai":["Pembicara/Nama Pertama/Instruktur","Panitia/Moderator","Peserta"]
-    }
-
-#penilaian
-penilaian=[["A",4.0],["AB",3.5],["B",3.0],["BC",2.5],["C",2.0]]
+    "pelaksanaan": ["Individu", "Kelompok Sd. 3 Orang", "Kelompok >3 Orang", "Pengurus"],
+    "tingkat": ["", "Nasional", "Internasional"],
+    "disiplin": ["Tidak Berhubungan", "Berhubungan"],
+    "rentang_waktu": ["<1 Bulan", "Sd. 3 Bulan", "Sd. 6 Bulan", "Sd. 1 Tahun", ">1 Tahun"],
+    "nilai": ["Pembicara/Nama Pertama/Instruktur", "Panitia/Moderator", "Peserta"]
+}
+# Kompetisi, Magang, dan Wirausaha
+kmw = {"kompetisi": 1, "magang": 1, "wirausaha": 1}
+# penilaian
+penilaian = [["A", 4.0], ["AB", 3.5], ["B", 3.0], ["BC", 2.5], ["C", 2.0]]
 
 
 class skem:
     def __init__(self):
-        #init variable nya
+        # init variable nya
         self.kredit = 1
         self.final = 1
         self.nilai_angka = 1.0
-        #ini penilaian yang A,BC gitu laaa owkwokwo:v
-        self.penilaian=[["A",4.0],["AB",3.5],["B",3.0],["BC",2.5],["C",2.0]]
-        
-    def nilai_skem(self,tipe):
-        #tipe ini dictionary dari isi kredit itu
+        # ini penilaian yang A,BC gitu laaa owkwokwo:v
+        self.penilaian = [["A", 4.0], ["AB", 3.5], ["B", 3.0], ["BC", 2.5], ["C", 2.0]]
+
+    def nilai_skem(self, tipe):
+        # tipe ini dictionary dari isi kredit itu
         self.kredit_desc = []
         self.nama_kegiatan = input("nama kegiatan: ")
         for quest in tipe:
             for i in range(len(tipe[quest])):
-                #nge print data dari dictionary
-                print(i+1, "=> " + tipe[quest][i])
+                # nge print data dari dictionary
+                print(i + 1, "=> " + tipe[quest][i])
             x = int(input("=>"))
-            if quest!="nilai":
-                #jika variabel quest gak "nilai"
-                #final itu nilai akhirnya yang NA*kredit
+            if quest != "nilai":
+                # jika variabel quest gak "nilai"
+                # final itu nilai akhirnya yang NA*kredit
                 self.final *= x
-                #kredit desc ini isinya data tentang kredit
-                self.kredit_desc.append(tipe[quest][x-1])
+                # kredit desc ini isinya data tentang kredit
+                self.kredit_desc.append(tipe[quest][x - 1])
                 self.kredit = self.final
             else:
-                self.final *= penilaian[x-1][1]
-                self.nilai_angka = penilaian[x-1][1]
-                self.nilai_huruf = penilaian[x-1][0]
-    def csv_row(self):
-        self.csv = '"'+self.nama_kegiatan+'",'+str(self.kredit)+',"'+self.nilai_huruf+'",'+str(self.nilai_angka)+','+str(self.final)
-        return self.csv
-        
+                self.final *= penilaian[x - 1][1]
+                self.nilai_angka = penilaian[x - 1][1]
+                self.nilai_huruf = penilaian[x - 1][0]
 
+    def csv_row(self):
+        self.csv = '"' + self.nama_kegiatan + '",' + str(self.kredit) + ',"' + self.nilai_huruf + '",' + str(
+            self.nilai_angka) + ',' + str(self.final)
+        return self.csv
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import classprog as cp
 skem = cp.skem()
-skem.nilai_skem(cp.isr)
+skem.nilai_skem(cp.peng_mas)
 print(skem.final)
 a = input()
